8a1df34021195fd18d65c3fd93eff97ca2a56d20
drooling <97707885+drooling@users.noreply.github.com>
2022-06-03 12:55:45 -0500

    added first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+mockData = [
+    {name: "Mark"},
+    {name: "Jill"}
+]
+
+app.get("/users", function (req, res) {
+    res.json({
+        success: true,
+        message: "Successfully fetched users, nice !",
+        users: mockData
+    })
+})
+
 app.listen(80, function () {
     console.log("Server running");
 })
